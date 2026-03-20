30ad27dbc0aeeb3bd3790472540d0eaf8db66a46
Aymane El M'hamdi <aymanelmhamdi123@gmail.com>
2026-03-20 00:15:44 +0000

    =

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -24,6 +24,9 @@ export default function Home() {
   if (error) return <p className="text-center mt-5">{error}</p>;
   const featuredMovie = movies[0];
   const highQualityPoster = featuredMovie?.image?.original.replace("SX300", "SX700");
+  const cleanSummary = featuredMovie?.summary
+  ?.replace(/<[^>]+>/g, "")
+  ?.replace(/\.\s*/g, ".\n");
 
   return (
     <div className='flex flex-col font-sans  min-h-screen max-w-[1550px] mx-auto'>
@@ -31,37 +34,50 @@ export default function Home() {
         <img
           src={highQualityPoster}
           alt={highQualityPoster}
-          className="w-[101rem] h-[41rem] object-cover scale-110 object-[50%_53%]"
+          className="w-[101rem] h-[41rem] object-cover  brightness-50 scale-110 object-[50%_53%]"
         />
         <div className="absolute top-0 left-0  w-full h-full flex flex-col justify-center mt-[8rem] ml-9">
-          <img
-            src={highQualityPoster}
-            alt={highQualityPoster}
-            className="h-[16rem] w-[12rem] pb-2 "
+          <div className="flex  gap-5 ">
+          <div className="flex flex-row">
+            <img
+              src={highQualityPoster}
+              alt={highQualityPoster}
+              className="h-[17rem] w-[13rem] pb-2 "
 
 
-          />
-          <div className="flex flex-row justify-between">
-            <div>
+            />
 
-              <h1 className="text-3xl ">
-                {featuredMovie?.name}
-              </h1>
-              <p className="text-3xl">
-                Year: {featuredMovie?.premiered}
+          </div>
+          <div className="flex justify-center  text-lg pr-[11rem] pt-[4rem] whitespace-pre-line text-white max-w-lg">
+            <p className="">{cleanSummary}</p>
 
-              </p>
+          </div>
+
+          </div>
+
+
+          <div className="flex flex-col">
+            <div className="flex flex-row  ">
+              <div className="flex flex-col">
+
+                <h1 className="text-3xl ">
+                  {featuredMovie?.name}
+                </h1>
+                <p className="text-3xl">
+                  Year: {featuredMovie?.premiered}
+
+                </p>
+                <button className="mt-5 bg-red-600 px-6 py-2 rounded w-[150px]">
+                  ▶ Play
+                </button>
+
+              </div>
+
+
+
 
             </div>
 
-
-            <div>
-
-
-              <button className="mt-5 bg-red-600 px-6 py-2 rounded w-[150px]">
-                ▶ Play
-              </button>
-            </div>
           </div>
 
 
@@ -83,7 +99,7 @@ export default function Home() {
         <div className="flex  flex-wrap justify-center gap-4">
           <div className="flex  flex-wrap justify-center gap-4 bg-gray-900 p-5 rounded-lg">
 
-            {movies.slice(0, 5).map((movie) => (
+            {movies.slice(0, 6).map((movie) => (
               <CardMovie key={movie.imdbID} movie={movie} />
 
 
